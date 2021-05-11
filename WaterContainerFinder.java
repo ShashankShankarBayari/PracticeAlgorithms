40a3f84c5e46614ae98b2325e48830bf5cca4d5c
ShashankShankarBayari <84006273+ShashankShankarBayari@users.noreply.github.com>
2021-05-11 09:12:55 +0200

    Create WaterContainerFinder.java

---
+++ WaterContainerFinder.java
@@ -0,0 +1,28 @@
+package com.sap.grc.issueandremediationmanagement.api.businessrules;
+
+import java.util.List;
+
+public class WaterContainerFinder {
+
+    public int findmaxWaterCapacity(List<Integer> wallHeightsList) {
+        int maximumCapacity = 0, currentCapacity = 0;
+        int leftWall = 0;
+        int rightWall = leftWall + 1;
+
+        while (rightWall < wallHeightsList.size()) {
+
+            if (wallHeightsList.get(leftWall) < wallHeightsList.get(rightWall)) {
+                currentCapacity = wallHeightsList.get(leftWall) * (rightWall - leftWall);
+                leftWall = rightWall;
+                rightWall = rightWall + 1;
+            } else {
+                currentCapacity = wallHeightsList.get(rightWall) * (rightWall - leftWall);
+                rightWall++;
+            }
+            maximumCapacity = currentCapacity > maximumCapacity ? currentCapacity : maximumCapacity;
+        }
+
+        return maximumCapacity;
+
+    }
+}
