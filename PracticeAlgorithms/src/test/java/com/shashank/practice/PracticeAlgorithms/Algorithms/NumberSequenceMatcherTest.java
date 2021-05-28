99936cf55e89ea621f9947ad41aca07c75e7670d
I077373 <shashank.shankar.bayari@sap.com>
2021-05-28 16:51:12 +0200

    Init

---
+++ PracticeAlgorithms/src/test/java/com/shashank/practice/PracticeAlgorithms/Algorithms/NumberSequenceMatcherTest.java
@@ -0,0 +1,12 @@
+package com.shashank.practice.PracticeAlgorithms.Algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberSequenceMatcherTest {
+
+    @Test
+    void validate() {
+    }
+}
---
+++ PracticeAlgorithms/src/test/java/com/shashank/practice/PracticeAlgorithms/Algorithms/PhoneKeyWordMatcherTest.java
@@ -0,0 +1,12 @@
+package com.shashank.practice.PracticeAlgorithms.Algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PhoneKeyWordMatcherTest {
+
+    @Test
+    void findWordsMatchingPhoneNumber() {
+    }
+}
